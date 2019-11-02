1ab4badffacc496f0d8d9d5f1676be8c2a32483c
blockChaim <47686371+hzaurzli@users.noreply.github.com>
2019-11-02 14:47:51 +0800

    Update nls.R

--- nls.R
+++ nls.R
@@ -1,10 +1,41 @@
+ds = read.table("/home/lirz/cutoff/genes.fpkm_table",sep = '\t',header = T)
+row.names(ds) = ds$tracking_id
+ds =ds[,-1]
+
 ee = c(0,0.16,0.32,0.48,0.64,0.8,1.6,2.4,3.2,4,4.8,5.6,6.4,7.2,8)
-uu = c(0,14863,16630,17231,17539,17681,17982,18037,18052,18085,18105,18084,18103,18105,18093)
-ii = round(uu/18280,4)
+uu = select_fpkm(ds)
+uu = c(0,uu)
+ii = round(uu/18300,4)
 data = as.data.frame(cbind(ee,uu,ii))
+
+
+select_fpkm <- function(x) {
+  aa = c()
+  for (i in 1:14) {
+    gt = x[which(x[,i] > 1),]
+    num = nrow(gt)
+    aa[i] = num
+    
+  }
+  return(aa)
+}
+
+
+sel_fpkm <- function(x) {
+  aa = c()
+  for (i in 1:14) {
+    gt = x[which(x[,i] > 0),]
+    num = nrow(gt)
+    aa[i] = num
+    
+  }
+  return(aa)
+}
+
 
 f <- nls(uu ~ -a/(ee+c) + b,start=list(a = 400, b = -49, c = 0.025), data=data, trace=T)
 
+library(ggplot2)
 library(scales)
 ggplot(data,aes(ee, uu))+
   geom_point(size=3)+geom_line(aes(ee,fitted(f)),col='red')+
